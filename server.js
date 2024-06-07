fd6782026d16e85bfb18e3a2f4ed8e420f25a5a6
Sharada Shenoy <142988714+sharadashenoy18@users.noreply.github.com>
2024-06-07 17:19:03 +0000

    done

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const http = require("http");
+const port = 8081; // local port num
 
-const port = 8081;
+// HTTP Methods
+
+/*
+>> GET: Inorder to get data from server
+>> POST: Sending data to server
+>> DELETE: Deleting the data from database
+>> PATCH: Updating certain fields
+>> PUT: Full Update
+*/
+
+const toDoList = ["learn", "apply things", "succeed"];
+
 http
-    .createServer((req,res) => {
-        res.writeHead(200, {"Content-Type" : "text/html"});
-        res.write("<h2>hiiiii hello</h2>");
-        res.end();
+  .createServer((req, res) => {
+    // call back func
+    const { method, url } = req;
+    // console.log(method, url);
+    if (url === "/todos") {
+      if (method === "GET") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(toDoList.toString());
+      }else{
+        res.writeHead(501);
+      }
     }
-    )
-    .listen(port , () => {
-        console.log(`NodeJs server Started Running on Port ${port}`);
-    })
+        res.end(); // End the response after writing the content
+      
+    // If the URL is not "/todos" or method is not GET, the response will be empty
+    // because we haven't written anything to it
+  })
+  .listen(port, () => {
+    // call back func
+    console.log(`NodeJs Server Started Running on Port: ${port}`);
+  });
